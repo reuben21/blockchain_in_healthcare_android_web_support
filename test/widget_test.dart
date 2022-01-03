f5f59214c1c41aacb3cd53abe0b26508cda99769
REUBEN COUTINHO <reuben211999@icloud.com>
2022-01-03 12:55:26 +0530

    first commit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:bic_android_web_support/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
